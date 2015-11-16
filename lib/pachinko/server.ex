a8c7bf22cf5c5fb64bd9a9a81bc2373a4ce0e70b
Reid Paape <wrpaape@gmail.com>
2015-11-15 19:20:46 -0600

    still figuring out api

--- lib/pachinko/server.ex
+++ lib/pachinko/server.ex
@@ -36,57 +36,54 @@ defmodule Pachinko.Server do
     {:ok, initial_state}
   end
 
-  def handle_cast({:ready, initial_state}, _from) do
-    {:update_state, ^printer_pid} =
-    __MODULE__
-    |> GenServer.call(:drop)
+  # all balls are live
+  # one ball drops into a bucket (dead) and is replaced by a new ball
+  def handle_call(:update_state, _from, {live_balls, buckets}) do
+      {live_balls, [dead_ball]} =
+        live_balls
+        |> Enum.split(-1)
+
+      next_balls =
+        [0 | shift(live_balls)]
+
+      next_buckets =
+        buckets
+        |> Map.update!(dead_ball, fn({count, full_blocks, remainder}) ->
+          case remainder do
+            7 -> {count + 1, full_blocks + 1, 0}
+            _ -> {count + 1, full_blocks, remainder + 1}
+          end
+        end)
+
+      next_state = {next_balls, next_buckets}
+
+      {:reply, next_state, next_state}
   end
 
-  def handle_call(:drop, _from, current_value) do
-
+  # last of dead balls are dropped, the empty list will be dropped
+  # :update_state is called again to retreive a reply with next_state
+  def handle_call(:update_state, from, {[], live_balls, buckets}) do
+    handle_call(:update_state, from, {[live_balls, buckets})
   end
+
+  # balls are dropped into play one at a time
+  # buckets are still out of reach
+  def handle_call(:update_state, _from, {[live_ball | dead_balls], live_balls, buckets}) do
+    next_balls =
+      [live_ball | shift(live_balls)]
+
+    next_state = {next_balls, buckets}
+
+    {:reply, next_state, next_state}
+  end
+
+  def handle_cast(:)
+  
+  # helper functions
 
   defp generate_balls(num_balls), do: List.duplicate(0, num_balls)
 
-  def drop(live_balls, buckets}) do
-    receive do
-      {^printer_pid, :update_state} ->
-        {live_balls, [dead_ball]} =
-          live_balls
-          |> Enum.split(-1)
-
-        next_balls =
-          [0 | shift(live_balls)]
-
-        next_buckets =
-          buckets
-          |> Map.update!(dead_ball, fn({count, full_blocks, remainder}) ->
-            case remainder do
-              7 -> {count + 1, full_blocks + 1, 0}
-              _ -> {count + 1, full_blocks, remainder + 1}
-            end
-          end)
-
-        send(printer_pid, {:next_state, next_balls, next_buckets})
-
-        drop(next_balls, next_buckets)
-    end
-  end
-
-  def drop([], live_balls, buckets), do: drop(live_balls, buckets)
-  def drop([live_ball | dead_balls], live_balls, buckets) do
-    receive do
-      {^printer_pid, :update_state} ->
-        next_balls =
-          [live_ball | shift(live_balls)]
-
-        send(printer_pid, {:next_state, next_balls, buckets})
-
-        drop(dead_balls, next_balls, buckets)
-    end
-  end
-
-  def flip_coin do
+  defp flip_coin do
     rand = :rand.uniform
     cond do
       rand > 0.5 -> :heads
@@ -95,7 +92,7 @@ defmodule Pachinko.Server do
     end
   end
 
-  def shift(balls) do
+  defp shift(balls) do
     balls
     |> Enum.map(fn(pos) ->
       pos + if flip_coin == :heads, do: 1, else: -1
